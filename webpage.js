8c9ec125073136e7dcff2ac0834c3dee86809db6
alicex28 <42008634+alicex28@users.noreply.github.com>
2018-08-01 14:33:06 -0400

    Add files via upload

---
+++ webpage.js
@@ -0,0 +1,20 @@
+function myFunction() {
+    var x = document.getElementById("div1");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+
+function myName() {
+    var txt;
+    var person = prompt("Please enter your name:", "Harry Potter");
+    if (person == null || person == "") {
+        txt = "User cancelled the prompt.";
+    } else {
+        txt = "Hello " + person + "!";
+    }
+    document.getElementById("demo").innerHTML = txt;
+}
